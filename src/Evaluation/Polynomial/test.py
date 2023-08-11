382fb3a59fd250d5a5c8e913571b66baff13e54f
Roman Parak <Roman.Parak@outlook.com>
2023-08-11 19:50:48 +0200

    improvements of the repo.

--- src/Evaluation/Polynomial/test.py
+++ src/Evaluation/Polynomial/test.py
@@ -17,76 +17,6 @@ import Lib.Trajectory.Core as Trajectory
 # Custom Script:
 #   ../Lib/Transformation/Utilities/Mathematics
 import Lib.Transformation.Utilities.Mathematics as Mathematics
-
-
-from math import comb, factorial
-import numpy as np
-
-
-class PolynomialGenerator():
-    """
-    PolynomialGenerator generates polynomial trajectories from constraints
-    """
-
-    def __init__(self):
-        pass
-
-    def generate_coefficients(self, q_init, q_final, t_f, t_0=0.0):
-        """
-        Calculates polynomial trajectory coefficients from constraints
-
-        Args:
-            q_init (list): list of constraints: [q_init, dq_init, ddq_init]
-            q_final (list): list of constraints: [q_final, dq_final, ddq_final]
-            t_f (float): Finish time
-            t_0 (float, optional): Start time
-
-        Returns:
-            np.array: Resultant polynomial coefficients
-        """
-        A_0 = self._get_constraint_submatrix(t_0)
-        A_f = self._get_constraint_submatrix(t_f)
-
-        A = np.vstack((A_0, A_f))
-        q = np.hstack((np.array(q_init), np.array(q_final)))
-
-        return np.linalg.inv(A).dot(q)
-
-    def _get_constraint_submatrix(self, t):
-        """
-        Generates time constraint submatrix for time t
-
-        Args:
-            t (float): Time constraint
-
-        Returns:
-            np.ndarray: Time constraint submatrix
-        """
-        A = np.array([[1, t, t**2, t**3, t**4, t**5],
-                      [0, 1, 2 * t, 3 * t**2, 4 * t**3, 5 * t**4],
-                      [0, 0, 2, 6 * t, 12 * t**2, 20 * t**3]])
-        return A
-
-    def polynomial_from_coefs(self, coefs, from_t, to_t, n):
-        """
-        Generates discrete polynomial of len(coefs) - 1 degree of size n
-
-        Args:
-            coefs (array-like): Coefficients of the polynomial to generate
-            from_t (float): Start point
-            to_t (float): End point
-            n (int): Number of points
-
-        Returns:
-            np.ndarray: Array of n points from the polynomial
-        """
-        ts = np.linspace(from_t, to_t, n)
-        poly = 0
-
-        for i, coef in enumerate(coefs):
-            poly += coef * ts**i
-
-        return poly
     
 """
 Description:
@@ -110,11 +40,6 @@ def main():
     Polynomial_Cls = Trajectory.Polynomial_Profile_Cls(thetas_0, thetas_1, 100)
     Polynomial_Cls.Generate()
 
-    pg = PolynomialGenerator()
-    coeff = pg.generate_coefficients([0.0,0.0,0.0], [1.57,0.0,0.0], 1.0, 0.0)
-    q = pg.polynomial_from_coefs(coeff, 0.0, 1.0, 100)
-
-
     # Set the parameters for the scientific style.
     plt.style.use(['science'])
 
@@ -125,8 +50,6 @@ def main():
     for i, s_theta_i in enumerate(Polynomial_Cls.s.T):
         ax.plot(Polynomial_Cls.t, s_theta_i, '.--', linewidth=1.0, markersize = 3.0, 
                 markeredgewidth = 1.5, label=r'$\theta_{%d}$' % (i + 1))
-
-    ax.plot(Polynomial_Cls.t, q)
 
     # Set parameters of the graph (plot).
     ax.set_title(f'Trajectory Polynomial Profile: Position', fontsize=25, pad=25.0)
