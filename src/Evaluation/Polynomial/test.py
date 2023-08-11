a75e13446e90eceebc9afb6b7696663d21c58c22
Roman Parak <Roman.Parak@outlook.com>
2023-08-11 18:20:23 +0200

    improvements of the repo.

--- src/Evaluation/Polynomial/test.py
+++ src/Evaluation/Polynomial/test.py
@@ -7,6 +7,10 @@ if '../' + 'src' not in sys.path:
 import numpy as np
 # OS (Operating system interfaces)
 import os
+# SciencePlots (Matplotlib styles for scientific plotting) [pip3 install SciencePlots]
+import scienceplots
+# Matplotlib (Visualization) [pip3 install matplotlib]
+import matplotlib.pyplot as plt
 # Custom Script:
 #   ../Lib/Trajectory/Core
 import Lib.Trajectory.Core as Trajectory
@@ -15,6 +19,75 @@ import Lib.Trajectory.Core as Trajectory
 import Lib.Transformation.Utilities.Mathematics as Mathematics
 
 
+from math import comb, factorial
+import numpy as np
+
+
+class PolynomialGenerator():
+    """
+    PolynomialGenerator generates polynomial trajectories from constraints
+    """
+
+    def __init__(self):
+        pass
+
+    def generate_coefficients(self, q_init, q_final, t_f, t_0=0.0):
+        """
+        Calculates polynomial trajectory coefficients from constraints
+
+        Args:
+            q_init (list): list of constraints: [q_init, dq_init, ddq_init]
+            q_final (list): list of constraints: [q_final, dq_final, ddq_final]
+            t_f (float): Finish time
+            t_0 (float, optional): Start time
+
+        Returns:
+            np.array: Resultant polynomial coefficients
+        """
+        A_0 = self._get_constraint_submatrix(t_0)
+        A_f = self._get_constraint_submatrix(t_f)
+
+        A = np.vstack((A_0, A_f))
+        q = np.hstack((np.array(q_init), np.array(q_final)))
+
+        return np.linalg.inv(A).dot(q)
+
+    def _get_constraint_submatrix(self, t):
+        """
+        Generates time constraint submatrix for time t
+
+        Args:
+            t (float): Time constraint
+
+        Returns:
+            np.ndarray: Time constraint submatrix
+        """
+        A = np.array([[1, t, t**2, t**3, t**4, t**5],
+                      [0, 1, 2 * t, 3 * t**2, 4 * t**3, 5 * t**4],
+                      [0, 0, 2, 6 * t, 12 * t**2, 20 * t**3]])
+        return A
+
+    def polynomial_from_coefs(self, coefs, from_t, to_t, n):
+        """
+        Generates discrete polynomial of len(coefs) - 1 degree of size n
+
+        Args:
+            coefs (array-like): Coefficients of the polynomial to generate
+            from_t (float): Start point
+            to_t (float): End point
+            n (int): Number of points
+
+        Returns:
+            np.ndarray: Array of n points from the polynomial
+        """
+        ts = np.linspace(from_t, to_t, n)
+        poly = 0
+
+        for i, coef in enumerate(coefs):
+            poly += coef * ts**i
+
+        return poly
+    
 """
 Description:
     Initialization of constants.
@@ -31,7 +104,53 @@ def main():
     # Locate the path to the project folder.
     project_folder = os.getcwd().split('Trajectory_Generation')[0] + 'Trajectory_Generation'
 
-    Polynomial_Cls = Trajectory.Polynomial_Profile_Cls(0.0, 1.57, 10)
+    thetas_0 = Mathematics.Degree_To_Radian(np.array([ 0.0], dtype=np.float32))
+    thetas_1 = Mathematics.Degree_To_Radian(np.array([90.0], dtype=np.float32))
+
+    Polynomial_Cls = Trajectory.Polynomial_Profile_Cls(thetas_0, thetas_1, 100)
+    Polynomial_Cls.Generate()
+
+    pg = PolynomialGenerator()
+    coeff = pg.generate_coefficients([0.0,0.0,0.0], [1.57,0.0,0.0], 1.0, 0.0)
+    q = pg.polynomial_from_coefs(coeff, 0.0, 1.0, 100)
+
+
+    # Set the parameters for the scientific style.
+    plt.style.use(['science'])
+
+    # Create a figure.
+    _, ax = plt.subplots()
+
+    # Visualization of ....
+    for i, s_theta_i in enumerate(Polynomial_Cls.s.T):
+        ax.plot(Polynomial_Cls.t, s_theta_i, '.--', linewidth=1.0, markersize = 3.0, 
+                markeredgewidth = 1.5, label=r'$\theta_{%d}$' % (i + 1))
+
+    ax.plot(Polynomial_Cls.t, q)
+
+    # Set parameters of the graph (plot).
+    ax.set_title(f'Trajectory Polynomial Profile: Position', fontsize=25, pad=25.0)
+    #   Set the x ticks.
+    ax.set_xticks(np.arange(np.min(Polynomial_Cls.t) - 0.1, np.max(Polynomial_Cls.t) + 0.1, 0.1))
+    #   Set the y ticks.
+    ax.set_yticks(np.arange(np.min(np.append(thetas_0, thetas_1)) - 0.1, np.max(np.append(thetas_0, thetas_1)) + 0.1, 0.314))
+    #   Label
+    ax.set_xlabel(r'Normalized Time ', fontsize=15, labelpad=10)
+    ax.set_ylabel(r'$\theta_{i}$ in radians', fontsize=15, labelpad=10) 
+    #   Set parameters of the visualization.
+    ax.grid(which='major', linewidth = 0.15, linestyle = '--')
+    # Get handles and labels for the legend.
+    handles, labels = plt.gca().get_legend_handles_labels()
+    # Remove duplicate labels.
+    legend = dict(zip(labels, handles))
+    # Show the labels (legends) of the graph.
+    ax.legend(legend.values(), legend.keys(), fontsize=10.0)
+
+    if CONST_SAVE_DATA == True:
+        pass
+    else:
+        # Show the result.
+        plt.show()
 
 if __name__ == "__main__":
     sys.exit(main())
--- src/Lib/Trajectory/Core.py
+++ src/Lib/Trajectory/Core.py
@@ -18,10 +18,18 @@ CONST_T_0 = 0.0
 CONST_T_1 = 1.0
 
 class Polynomial_Profile_Cls(object):
-    def __init__(self, s_0: tp.Union[float, tp.List[float]], s_1: tp.Union[float, tp.List[float]], N: int) -> None:
+    def __init__(self, s_0: tp.List[float], s_1: tp.List[float], N: int) -> None:
         # The value of the time must be within the interval: 
         #   0.0 <= t <= 1.0
         self.__t = np.linspace(CONST_T_0, CONST_T_1, N)
+
+        self.__s_0 = s_0
+        self.__s_1 = s_1
+
+        # Initialization of other class parameters.
+        self.__s = np.zeros((N, s_0.size), dtype=np.float32)
+        self.__s_dot  = self.__s.copy()
+        self.__s_ddot = self.__s.copy()
 
     @property
     def t(self) -> tp.List[float]:
@@ -50,19 +58,41 @@ class Polynomial_Profile_Cls(object):
         return self.__t.shape[0]
     
     @property
-    def s(self):
-        return None
+    def s(self): 
+        return self.__s
     
     @property
     def s_dot(self):
-        return None
+        return self.__s_dot
     
     @property
     def s_ddot(self):
-        return None
+        return self.__s_ddot
     
+    def __Quintic_Polynomial(self, s_0, s_1):
+        s_dot_0 = 0.0; s_dot_1 = 0.0
+
+        A = 6.0 * (s_1 - s_0) - 3 * (s_dot_1 + s_dot_0)
+        B = -15.0 * (s_1 - s_0) + (8 * s_dot_0 + 7 * s_dot_1)
+        C = 10.0 * (s_1 - s_0) - (6 * s_dot_0 + 4 * s_dot_1)
+        E = s_dot_0
+        F = s_0
+
+        # ...
+        t = np.array([self.__t ** 5, self.__t ** 4, self.__t ** 3, 
+                      self.__t ** 2, self.__t, np.ones(self.N)], dtype=np.float32).T
+        
+        return (t @ np.array([  A,       B,        C,      0.0,       E,   F], dtype=np.float32),
+                t @ np.array([0.0, 5.0 * A,  4.0 * B,  3.0 * C,     0.0,   E], dtype=np.float32),
+                t @ np.array([0.0,     0.0, 20.0 * A, 12.0 * B, 6.0 * C, 0.0], dtype=np.float32))
+
     def Generate(self):
-        return True
+        for i, (s_0_i, s_1_i) in enumerate(zip(self.__s_0, self.__s_1)):
+            # ...
+            (s, s_dot, s_ddot) = self.__Quintic_Polynomial(np.array(s_0_i, dtype=np.float32), 
+                                                           np.array(s_1_i, dtype=np.float32))
+            # ...
+            self.__s[:, i] = s; self.__s_dot[:, i] = s_dot; self.__s_ddot[:, i] = s_ddot
     
 class Trapezoidal_Profile_Cls(object):
     def __init__(self) -> None:
